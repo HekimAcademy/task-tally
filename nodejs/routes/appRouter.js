3311ee158c94cad6b24a88da8e824ea1611c8e8d
0xWindsor <keremeksioglu42@gmail.com>
2023-07-02 17:00:55 +0300

    routed updated with new functions

--- nodejs/routes/appRouter.js
+++ nodejs/routes/appRouter.js
@@ -2,8 +2,8 @@ const Router = require('express').Router()
 
 
 const { firebaseSignUp, firebaseSignIn } = require('../controller/authController')
-const { createProject, getProject, joinProject } = require('../controller/projectsController')
-const { useCronometer } = require('../controller/appController')
+const { createProject, getProject, getUserProjects, joinProject } = require('../controller/projectsController')
+const { useCronometer, getUserWorkLogs } = require('../controller/appController')
 
 const { validateFirebaseIdToken } = require('../controller/tokenMiddleware')
 
@@ -14,7 +14,10 @@ Router.post('/auth/signIn', firebaseSignIn)
 Router.post('/projects', validateFirebaseIdToken, createProject)
 Router.post('/projects/join', validateFirebaseIdToken, joinProject)
 Router.get('/projects/:projectId', validateFirebaseIdToken, getProject)
+Router.get('/projects/user/:userId', validateFirebaseIdToken, getUserProjects)
 
-Router.post('/cronometer', validateFirebaseIdToken, useCronometer)
+Router.post('/app/cronometer', validateFirebaseIdToken, useCronometer)
+Router.get('/app/:userId', validateFirebaseIdToken, getUserWorkLogs)
+
 
 module.exports = Router
