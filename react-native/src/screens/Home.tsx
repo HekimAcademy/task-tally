94edd88a6fb62176cf16d88275a98a9dc81a0ef2
0xWindsor <keremeksioglu42@gmail.com>
2023-07-02 23:07:29 +0300

    Home screen

--- react-native/src/screens/Home.tsx
+++ react-native/src/screens/Home.tsx
@@ -1,10 +1,30 @@
-import { Text, Button, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Button, Pressable, Text, View } from "react-native";
+import theme from "../styles/theme"
+import CronometerButtonComponent from "../components/cronometerButton";
+import SelectInputComponent from "../components/selectInput/selectInput";
 
 export default function HomeScreen() {
-    return(
+
+    const [state, setState] = useState("Start");
+
+    return (
         <View>
             <Text>Home Screen</Text>
-            <Button title="Deatils" />
+            <SelectInputComponent />
+            <View style={styles.container}>
+                <Button title="Deatils" />
+                <CronometerButtonComponent radius={250} state={state} setState={setState} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    }
+});
